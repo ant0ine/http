2865635394199eb61db9b7641e28802e6c051eb2
franck cuny <franck.cuny@gmail.com>
2012-01-03 21:19:45 -0800

    add Headers to export

--- fluffyhttp/__init__.py
+++ fluffyhttp/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ['Request', 'Client', 'Response', 'HTTPException']
+__all__ = ['Request', 'Client', 'Response', 'HTTPException', 'Headers']
 
 from client import Client
 from request import Request
 from response import Response
+from headers import Headers
 from exception import *
