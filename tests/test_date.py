b83e9f5b853b770d4718fbe0098fb5d43ab6894a
franck cuny <franck.cuny@gmail.com>
2012-01-15 14:52:50 -0800

    add some tests for fluffyhttp.Date

---
+++ tests/test_date.py
@@ -0,0 +1,28 @@
+from unittest2 import TestCase
+from datetime import datetime
+from fluffyhttp.date import Date
+import time
+
+
+class TestDate(TestCase):
+
+    def test_time2str(self):
+        str = Date.time2str(datetime(2011, 12, 12, 12, 0, 0))
+        self.assertEqual(str, 'Mon, 12 Dec 2011 12:00:00 GMT')
+
+    def test_str2time(self):
+        time = Date.str2time('Mon, 12 Dec 2011 12:00:00 GMT')
+        self.assertEqual(time.year, 2011)
+
+    def test_str2epoch(self):
+        epoch = Date.str2epoch('Mon, 12 Dec 2011 12:00:00 GMT')
+        self.assertEqual(epoch, 1323720000)
+
+    def test_epoch2time(self):
+        time = Date.epoch2time(1323720000)
+        self.assertEqual(time.year, 2011)
+        pass
+
+    def test_epoch2str(self):
+        str = Date.epoch2str(1323720000)
+        self.assertEqual(str, 'Mon, 12 Dec 2011 12:00:00 GMT')
