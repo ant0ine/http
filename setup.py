ec2258ddc1ed65441c31bd0106ce5b66dbb219c1
franck cuny <franck.cuny@gmail.com>
2012-01-22 15:55:10 -0800

    some changes to setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,18 @@ from os.path import join, dirname
 setup(
     name='http',
     version='0.1.0',
-    description='a HTTP library for Python',
+    description='HTTP library for Python',
+    long_description=open('readme.rst').read(),
     author='Franck Cuny',
     author_email='franck.cuny@gmail.com',
+    url='https://github.com/franckcuny/http',
     packages=['http'],
-    provides=['http'],
+    license='MIT',
     requires=['coverage', 'nose', 'unittest2', 'pep8'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+    ]
 )
