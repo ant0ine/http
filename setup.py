0820c22f14ac85917b981a276393dddb2ba23d2d
James Rowe <jnrowe@gmail.com>
2012-02-09 19:19:38 +0000

    Mark as compatible with Python 2.6/2.7 in setup.

--- setup.py
+++ setup.py
@@ -16,6 +16,8 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'License :: OSI Approved :: MIT License',
     ]
 )
